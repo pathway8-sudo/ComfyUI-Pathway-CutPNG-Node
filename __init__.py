0359372d884deec0cb0236187da14e60be40d48c
Pathway4 <kuraiondrumillaii@gmail.com>
2025-02-20 14:56:27 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
-from .cut_png_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+try:
+    from .cut_png_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+except Exception as e:
+    print("Error importing cut_png_node:", e)
+    raise e
